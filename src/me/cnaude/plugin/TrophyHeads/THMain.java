91d67c2b0da5131fe4f7319de25ece4c93118ec4
cnaude <cnaude@zebra.lan>
2013-01-31 21:02:12 -0700

    Added /headspawn command and permission nodes.

--- src/me/cnaude/plugin/TrophyHeads/THMain.java
+++ src/me/cnaude/plugin/TrophyHeads/THMain.java
@@ -9,9 +9,12 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +22,7 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -36,6 +40,8 @@ public class THMain extends JavaPlugin implements Listener {
     private static int dropChance = 100;
     private static ArrayList<String> deathTypes = new ArrayList<String>();
     private static boolean debugEnabled = false;
+    private static boolean playerSkin = true;
+    private static boolean sneakPunchInfo = true;
     
     @Override
     public void onEnable() {
@@ -48,15 +54,91 @@ public class THMain extends JavaPlugin implements Listener {
         saveConfig();
         loadConfig();
         getServer().getPluginManager().registerEvents(this, this);   
+        getCommand("headspawn").setExecutor(this);
     }
         
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.hasPermission("trophyheads.spawn")) {
+                String pName = player.getName();
+                int count = 1;
+                if (args.length >= 1) {
+                    pName = args[0];                    
+                    if (args.length == 2) {
+                        if (args[1].matches("\\d+")) {
+                            count = Integer.parseInt(args[1]);                        
+                        }
+                    }
+                }                
+                ItemStack item = new ItemStack(Material.SKULL_ITEM, count, (byte) 3);
+                Location loc = player.getLocation().clone();
+                World world = loc.getWorld();
+                ItemMeta itemMeta = item.getItemMeta();
+                ((SkullMeta)itemMeta).setOwner(pName);
+                item.setItemMeta(itemMeta);
+                world.dropItemNaturally(loc,item); 
+                
+            } else {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                
+            }
+        } else {
+            sender.sendMessage("Only a player can use this command!");
+        }
+        return true;
+    }
+    
+    /*
     @EventHandler
-    public void onPlayerDeathEvent(PlayerDeathEvent event) {                                            
-        if (randomGenerator.nextInt(100) > dropChance) {
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {        
+        if (!sneakPunchInfo) {            
             return;
         }
-        
+        Player player = event.getPlayer();
+        if (!player.isSneaking()) {            
+            return;
+        }
+        Block block = player.getTargetBlock(null, 5);
+        if (block == null) {
+            block = event.getClickedBlock();
+        }        
+        if (block.getType() == Material.SKULL) {
+            logDebug("Player sneak clicked on a valid Skull");
+            org.bukkit.block.Skull skull = (org.bukkit.block.Skull)block;
+            String pName = "Unknown";            
+            try {
+                logDebug("About to check for owner!");
+                if (skull.hasOwner()) {
+                    logDebug("Skull block has owner");
+                    pName = skull.getOwner();
+                } else {
+                    logDebug("Skull block has NO owner");
+                }
+                player.sendMessage(ChatColor.YELLOW + "This head once belonged to " + ChatColor.RED + pName);
+            }
+            catch (Exception e) {
+                logError(e.getMessage());
+            }
+            finally {
+                logDebug("Finally!");
+            }
+        } else {
+            logDebug("Player sneak clicked on an invalid Skull");
+        }
+    }
+    */
+    
+    @EventHandler
+    public void onPlayerDeathEvent(PlayerDeathEvent event) {        
+        if (randomGenerator.nextInt(100) > dropChance) {
+            return;
+        }        
         Player player = (Player)event.getEntity();
+        if (!player.hasPermission("trophyheads.drop")) {
+            return;
+        }
         DamageCause dc = player.getLastDamageCause().getCause();        
         logDebug("DamageCause: " + dc.toString());        
         
@@ -73,7 +155,10 @@ public class THMain extends JavaPlugin implements Listener {
             ArrayList<String> itemDesc = new ArrayList<String>();
             itemMeta.setDisplayName("Head of " + pName);
             itemDesc.add(event.getDeathMessage());
-            itemMeta.setLore(itemDesc);
+            itemMeta.setLore(itemDesc);            
+            if (playerSkin) {
+                ((SkullMeta)itemMeta).setOwner(pName);
+            }
             item.setItemMeta(itemMeta);
             world.dropItemNaturally(loc,item); 
         } else {
@@ -107,7 +192,11 @@ public class THMain extends JavaPlugin implements Listener {
         for (String type : getConfig().getStringList("death-types")) {
             deathTypes.add(type);                
             logDebug("Death type: " + type);
-        }             
+        }     
+        playerSkin = getConfig().getBoolean("player-skin");
+        logDebug("Player skins: " + playerSkin);
+        sneakPunchInfo = getConfig().getBoolean("sneak-punch-info");
+        logDebug("Sneak punch info: " + sneakPunchInfo);
     }
             
     public void logInfo(String _message) {
